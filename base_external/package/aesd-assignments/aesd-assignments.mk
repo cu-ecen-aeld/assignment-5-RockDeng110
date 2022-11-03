f5f20bb20b60991d26b653a283b0a4476681153a
RockDeng110 <dengyongpeng110@outlook.com>
2022-11-03 09:43:54 +0800

    change the commit of assignment3 repo to support make all in the finder app

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 567c0688c0772189f996d12029b29f416b04e266
+AESD_ASSIGNMENTS_VERSION = 6ecbf3ffa9f3f9e59ed8bf41275db15b62692515
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
